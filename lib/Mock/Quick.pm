331b23090f975d82dcc50b1c8dcf295848b9bb60
Chad Granum <exodist7@gmail.com>
2011-04-02 20:55:32 -0700

    Version bump

--- lib/Mock/Quick.pm
+++ lib/Mock/Quick.pm
@@ -8,7 +8,7 @@ use Mock::Quick::Object::Control;
 use Mock::Quick::Method;
 use Mock::Quick::Util;
 
-our $VERSION = '1.001';
+our $VERSION = '1.100';
 
 default_export qclass     => sub { Mock::Quick::Class->new( @_ )           };
 default_export qtakeover  => sub { Mock::Quick::Class->takeover( @_ )      };
